eb71bc3c29acce931b3d35ba2e307cd3df902ff3
Lana Ryzhkova <Lana Ryzhkova@LAPTOP-12RH4JAG>
2021-11-09 14:01:38 +0300

    Добавьте файлы проекта.

---
+++ laba1/Entities/ServicePartial.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace laba1.Entities
+{
+   public partial class Service
+    {
+        public string DiscountText
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "";
+                else
+                    return $"* скидка {Discount * 100}%";
+            }
+        }
+        public string TotalCost
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                {
+                    return $"{Cost:N2} рублей за {DurationInSeconds / 60} минут";
+                }
+                else
+                {
+                    return $"{CostWithDiscount:N2} рублей за {DurationInSeconds / 60} минут";
+                }
+            }
+        }
+        public double CostWithDiscount
+        {
+            get
+            {
+                if (Discount==0||Discount==null)
+                {
+                    return (double)Cost;
+                }
+                else
+                {
+                    var costWithDiscount = (double)Cost * (1.00 - Discount);
+                    return costWithDiscount.Value;
+                }
+            }
+        }
+        public Visibility DiscountVisibility
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+
+        }
+        public string BackColor
+        {
+            get
+            {
+                if (Discount == 0 || Discount == null)
+                    return "#FFFFE1";
+                else
+                    return "#D1FFD1";
+            }
+        }
+        public string AdminControlIsVisibility
+        {
+            get
+            {
+                if (App.CurrentUser.RoleId ==1)
+                {
+                    return "Visible";
+                }
+                else
+                {
+                    return "Collapsed";
+                }
+            }
+        }
+    }
+}
---
+++ laba1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FrameMain.Navigate(new Pages.LoginPage());
+
+            var clientsData = File.ReadAllLines(@"D:\учёба\фак\Клиенты.txt");
+            for (int i=0; i<clientsData.Count();i++)
+            {
+                var currentClient = clientsData[i].Split('\t');
+
+                var clientForDB = new Client
+                {
+                    LastName = currentClient[0],
+                    FirstName = currentClient[1],
+                    Patronimyc = currentClient[2],
+                    GenderCode = currentClient[3],
+                    Phone = currentClient[4],
+                    Birthday = DateTime.Parse(currentClient[5]),
+                    Email = currentClient[6],
+                    RegistrationDate = DateTime.Parse(currentClient[7])
+                };
+            }
+
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (FrameMain.CanGoBack)
+                FrameMain.GoBack();
+        }
+
+    }
+}
